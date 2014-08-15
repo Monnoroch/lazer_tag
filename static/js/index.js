e8e8c2c8b4ee3bea43e2154b3904a286e80008a3
monnoroch <monnoroch@gmail.com>
2014-08-15 18:49:34 +0400

    refraction, interactive targets

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,30 @@
+Array.prototype.clone = function() {
+	return this.slice(0);
+};
+
 (function() {
 
 var levels = {
 	1: {
 		lasers: [{
-			color: 0xFF00FF,
+			color: 0x00FF00,
 			position: {x: 100, y: 300},
 			direction: {x: Math.sqrt(2)/2, y: -Math.sqrt(2)/2},
 			strength: 0.8,
 			enabled: true,
-		}],
+		},
+		// {
+		// 	color: 0xFF00FF,
+		// 	position: {x: 500, y: 400},
+		// 	direction: {x: -Math.sqrt(2)/2, y: -Math.sqrt(2)/2},
+		// 	strength: 0.8,
+		// 	enabled: true,
+		// }
+		],
 		targets: [{
-			color: 0x00FFFF,
+			color: 0x00FF00,
 			position: {x: 400, y: 300/2},
+			radius: 20,
 		}],
 		prismas: [{
 			color: 0x00FF00,
@@ -22,18 +35,144 @@ var levels = {
 			],
 			position: {x: 640/2, y: 480/2-30},
 			opacity : 0.5,
+			coeff: 1.58,
 		}, {
 			color: 0x00FF00,
 			points: [
 				{x: 0, y: -20, color: 0x0000FF},
-				{x: -30, y: 20},
+				{x: -30, y: 20, opacity: 0.1},
 				{x: 30, y: 0},
 			],
 			position: {x: 100, y: 100},
 			opacity : 0,
+			coeff: 1.58,
 		}],
 	}
 };
+
+function getColor(num) {
+	return [0xFF0000, 0xFF8800, 0xFFFF00, 0x00FF00, 0x00FFFF, 0x0000FF, 0xFF00FF, 0xFFFFFF][num];
+}
+
+function getColorNum(color) {
+	for (var i = 0; i < 8; ++i) {
+		if (getColor(i) == color) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+function getLazerSpectrum(color, strength) {
+	var res = [0, 0, 0, 0, 0, 0, 0, 0];
+	res[getColorNum(color)] = strength;
+	return res;
+}
+
+function getLazerColorStrength(spectrum) {
+	var s = spectrum;
+	var res = [
+		s[0] + s[1]*3.0/4 + s[2]/2 + s[6]/2 + s[7]/3,
+		s[1] + s[0]/2 + s[2]/2 + s[3]/4 + s[7]/3,
+		s[2] + s[0]/2 + s[1]/2 + s[3]/2 + s[7]/3,
+		s[3] + s[1]*1.0/4 + s[2]/2 + s[4]/2 + s[7]/3,
+		s[4] + s[3]/2 + s[5]/2 + s[7]/3,
+		s[5] + s[4]/2 + s[6]/2 + s[7]/3,
+		s[6] + s[0]/2 + s[5]/2 + s[7]/3,
+		s[7] + s[0]/3 + s[1]/3 + s[2]/3 + s[3]/3 + s[4]/3 + s[5]/3 + s[6]/3,
+	];
+
+	var sum = 0;
+	var max = 0, maxi;
+	for (var i = 0; i < spectrum.length; ++i) {
+		var val = spectrum[i];
+		sum += val;
+		if (val > max) {
+			max = val;
+			maxi = i;
+		}
+	}
+	return {
+		col: getColor(maxi),
+		str: sum < 1e-4 ? 0 : res[maxi]/Math.max(sum, 1),
+	}
+}
+
+function getLazerStrength(spectrum, col) {
+	var s = spectrum;
+	var res = [
+		s[0] + s[1]*3.0/4 + s[2]/2 + s[6]/2 + s[7]/3,
+		s[1] + s[0]/2 + s[2]/2 + s[3]/4 + s[7]/3,
+		s[2] + s[0]/2 + s[1]/2 + s[3]/2 + s[7]/3,
+		s[3] + s[1]*1.0/4 + s[2]/2 + s[4]/2 + s[7]/3,
+		s[4] + s[3]/2 + s[5]/2 + s[7]/3,
+		s[5] + s[4]/2 + s[6]/2 + s[7]/3,
+		s[6] + s[0]/2 + s[5]/2 + s[7]/3,
+		s[7] + s[0]/3 + s[1]/3 + s[2]/3 + s[3]/3 + s[4]/3 + s[5]/3 + s[6]/3,
+	];
+
+	var sum = 0;
+	for (var i = 0; i < spectrum.length; ++i) {
+		sum += spectrum[i];
+	}
+
+	return sum < 1e-4 ? 0 : res[getColorNum(col)]/Math.max(sum, 1);
+}
+
+function weakenBeam(spectrum, color, opacity) {
+	var res = [];
+	var num = getColorNum(color);
+	for (var i = 0; i < spectrum.length; ++i) {
+		if (i == num) {
+			res.push(spectrum[i] * (1 - opacity));
+		} else {
+			res.push(spectrum[i] * (1 - (1 + opacity)/2));
+		}
+	}
+	return res;
+}
+
+function splitBeam(spectrum, color, opacity) {
+	// через белую стенку все проходит
+	if (color == 0xFFFFFF) {
+		if (getLazerColorStrength(spectrum).str < 0.01) {
+			spectrum = null;
+		}
+		return [null, spectrum];
+	}
+
+	var num = getColorNum(color);
+
+	var st = [0, 0, 0, 0, 0, 0, 0, 0];
+	st[num] = spectrum[num];
+	st[7] = spectrum[7];
+
+	// если совсем не можем пройти, то все отразится
+	if (getLazerColorStrength(st).str < 0.01) {
+		return [spectrum, null];
+	}
+
+	var s1 = spectrum.clone();
+	s1[num] *= opacity;
+	s1[7] *= opacity;
+
+	var s2 = [0, 0, 0, 0, 0, 0, 0, 0];
+	s2[num] = (1 - opacity) * spectrum[num];
+	s2[7] = (1 - opacity) * spectrum[7];
+
+	var str1 = getLazerColorStrength(s1).str;
+	var str2 = getLazerColorStrength(s2).str;
+
+	if (str1 < 0.01) {
+		s1 = null;
+	}
+
+	if (str2 < 0.01) {
+		s2 = null;
+	}
+
+	return [s1, s2];
+}
 
 var minOpacity = 0.3;
 
@@ -63,6 +202,7 @@ function graphicsOpacity(op) {
 	return op * (1 - minOpacity) + minOpacity;
 }
 
+var laserPointDist = 35;
 
 function createLaser(data, stage, world) {
 	var graphics = new PIXI.Graphics();
@@ -71,7 +211,7 @@ function createLaser(data, stage, world) {
 	graphics.beginFill(data.color, 0.5);
 
 	var vec = Physics.vector(data.direction.x, data.direction.y).normalize();
-	var p1 = vec.clone().mult(35);
+	var p1 = vec.clone().mult(laserPointDist);
 	var p2 = vec.clone().rotate(150*Math.PI/180).mult(25);
 	var p3 = vec.clone().rotate(210*Math.PI/180).mult(25);
 
@@ -95,19 +235,18 @@ function createLaser(data, stage, world) {
 }
 
 function createTarget(data, stage, world) {
-	var radius = 20;
 	var graphics = new PIXI.Graphics();
 	graphics.position = new PIXI.Point(data.position.x, data.position.y);
 	graphics.lineStyle(3, data.color, 1);
 	graphics.beginFill(data.color, minOpacity);
-	graphics.drawCircle(0, 0, radius);
+	graphics.drawCircle(0, 0, data.radius);
 	graphics.endFill();
-	graphics.shape = new PIXI.Circle(0, 0, radius);
+	graphics.shape = new PIXI.Circle(0, 0, data.radius);
 
 	graphics.body = Physics.body('circle', {
 		x: data.position.x,
 		y: data.position.y,
-		radius: radius,
+		radius: data.radius,
 	});
 	graphics.body.treatment = "static";
 	world.add(graphics.body);
@@ -347,6 +486,24 @@ function buildRibs(data) {
 	return res;
 }
 
+function isRibsEqual(ribs, oldRibs) {
+	if (!ribs || !oldRibs || ribs.length != oldRibs.length) {
+		return false;
+	}
+
+	for (var i = 0; i < ribs.length; ++i) {
+		var r = ribs[i];
+		var or = oldRibs[i];
+		if (r.num != or.num || r.prisma != or.prisma) {
+			return false;
+		}
+		if (!r.from.equals(or.from) || !r.to.equals(or.to)) {
+			return false;
+		}
+	}
+	return true;
+}
+
 
 function intersection(beam, rib) {
 	var p0 = beam.pos;
@@ -372,19 +529,79 @@ function intersection(beam, rib) {
 	return undefined;
 }
 
-function traceBeam(beam, ribs, world, res, depth) {
-	if (depth === undefined) {
-		depth = 1;
+function beamPointDist(beam, pt) {
+	var b = pt.clone().vsub(beam.pos).dot(beam.dir);
+	if (b < 0) {
+		return beam.pos.clone().vsub(pt).norm();
+	}
+	var a = ((beam.pos.x - pt.x) + b*beam.dir.x)/beam.dir.y;
+	var inters = beam.pos.clone().vadd(beam.dir.clone().mult(b));
+	return inters.vsub(pt).norm();
+}
+
+function beamCircleIntersection(beam, pt, rad) {
+	var d = beam.pos.clone().vsub(pt);
+	var b = beam.dir;
+	var dot = b.dot(d);
+	var s = rad*rad + dot*dot - d.normSq();
+	if (s < 0) {
+		return undefined;
+	}
+	if (s < 1e-4) {
+		return beam.pos.clone().vadd(beam.dir.clone().mult(-dot));
 	}
 
+	var sq = Math.sqrt(s);
+	var a1 = sq - dot;
+	var a2 = -sq - dot;
+
+	if (a1 < 0 && a2 < 0) {
+		return undefined;
+	}
+
+	if (a1 < 0) {
+		return beam.pos.clone().vadd(beam.dir.clone().mult(a2));
+	}
+
+	if (a2 < 0) {
+		return beam.pos.clone().vadd(beam.dir.clone().mult(a1));
+	}
+
+	var v1 = beam.dir.clone().mult(a1);
+	var v2 = beam.dir.clone().mult(a2);
+	if (v1.norm() < v2.norm()) {
+		return beam.pos.clone().vadd(v1);
+	} else {
+		return beam.pos.clone().vadd(v2);
+	}
+}
+
+function traceBeam(beam, ribs, world, res, depth, isInside, data) {
 	if (depth > 100) {
 		return;
 	}
 
-
 	var dist = 9999999;
 	var intersec = null;
 	var num = -1;
+	var gotTarget = null;
+	var numTarget = -1;
+
+	for (var i = 0; i < data.targets.length; ++i) {
+		var target = data.targets[i];
+		var is = beamCircleIntersection(beam, Physics.vector(target.position.x, target.position.y), target.radius);
+		if (is === undefined) {
+			continue;
+		}
+
+		var ds = beam.pos.clone().vsub(is).norm();
+		if (ds < dist) {
+			dist = ds;
+			gotTarget = is;
+			numTarget = i;
+		}
+	}
+
 	// get closest intersection
 	for (var i = 0; i < ribs.length; ++i) {
 		var is = intersection(beam, ribs[i]);
@@ -397,7 +614,18 @@ function traceBeam(beam, ribs, world, res, depth) {
 			dist = ds;
 			intersec = is;
 			num = i;
+			gotTarget = null;
 		}
+	}
+
+	if (gotTarget != null) {
+		res.push({
+			from: beam.pos.clone(),
+			to: gotTarget.clone(),
+			color: beam.color,
+		});
+		data.targets[numTarget].filled += getLazerStrength(beam.color, target.color);
+		return;
 	}
 
 	if (intersec == null) {
@@ -408,7 +636,6 @@ function traceBeam(beam, ribs, world, res, depth) {
 		from: beam.pos.clone(),
 		to: intersec.clone(),
 		color: beam.color,
-		strength: beam.strength,
 	});
 
 	if (ribs[num].border) {
@@ -419,18 +646,62 @@ function traceBeam(beam, ribs, world, res, depth) {
 	var rib = ribs[num];
 	var vec2 = rib.to.clone().vsub(rib.from);
 	var angle = Math.atan2(beam.dir.y, beam.dir.x) - Math.atan2(vec2.y, vec2.x);
-	var newBeam = {
-		pos: intersec.clone().vsub(beam.dir.clone().mult(0.1)),
-		dir: beam.dir.clone().rotate(2 * Math.abs(angle)).normalize(),
-		color: beam.color,
-		strength: beam.strength,
+	var angleWithNorm = -(Math.PI/2 + angle);
+
+	var pop = rib.prisma.opacity;
+	if (pop == undefined) {
+		pop = 1;
+	}
+
+	var reflectAngle = Math.PI + 2 * angleWithNorm; // отражение
+	var reflectOffset = beam.dir.clone().mult(-0.1);
+	var reflectBeam = {
+		pos: intersec.clone().vadd(reflectOffset),
+		dir: beam.dir.clone().rotate(reflectAngle).normalize(),
+		color: isInside ? weakenBeam(beam.color, rib.prisma.color, pop) : beam.color,
 	};
 
-	traceBeam(newBeam, ribs, world, res, depth + 1);
+	// n1*sin t1 == n2*sin t2
+	// внутрь n1 == 1
+	// наружу n2 == 1
+	var n = isInside ? rib.prisma.coeff / 1 : 1 / rib.prisma.coeff; // == n1/n2
+	var sint2 = n * Math.sin(Math.abs(angleWithNorm));
+
+	if (isInside && sint2 < -1 || sint2 > 1) {
+		traceBeam(reflectBeam, ribs, world, res, depth + 1, isInside, data);
+		return;
+	}
+
+	var op = rib.prisma.points[rib.num].opacity;
+	if (op == undefined) {
+		op = 1;
+	}
+	var beams = splitBeam(beam.color, rib.prisma.points[rib.num].color || rib.prisma.color, op);
+	if (beams[0] != null) {
+		reflectBeam.color = beams[0];
+		traceBeam(reflectBeam, ribs, world, res, depth + 1, isInside, data);
+	}
+
+	if (beams[1] != null) {
+		var refractAngle = Math.asin(sint2); // преломление
+		var refractOffset = beam.dir.clone().mult(0.1);
+		var refractBeam = {
+			pos: intersec.clone().vadd(refractOffset),
+			dir: beam.dir.clone().rotate(refractAngle).normalize(),
+			color: !isInside ? weakenBeam(beams[1], rib.prisma.color, pop) : beams[1],
+		};
+		traceBeam(refractBeam, ribs, world, res, depth + 1, !isInside, data);
+	}
+
+	// console.log(depth, angle / Math.PI * 180, angleWithNorm / Math.PI * 180, reflectAngle / Math.PI * 180, refractAngle / Math.PI * 180, n);
 }
 
 function calculateBeams(data, world) {
 	var ribs = buildRibs(data);
+	if (isRibsEqual(ribs, world.oldRibs)) {
+		return false;
+	}
+
 	var res = [];
 	for (var i = 0; i < levels[1].lasers.length; ++i) {
 		var lazer = levels[1].lasers[i];
@@ -438,14 +709,15 @@ function calculateBeams(data, world) {
 			continue;
 		}
 
+		var dir = Physics.vector(lazer.direction.x, lazer.direction.y).normalize();
 		var beam = {
-			pos: Physics.vector(lazer.position.x, lazer.position.y),
-			dir: Physics.vector(lazer.direction.x, lazer.direction.y).normalize(),
-			color: lazer.color,
-			strength: lazer.strength,
+			pos: Physics.vector(lazer.position.x, lazer.position.y).vadd(dir.clone().mult(laserPointDist)),
+			dir: dir,
+			color: getLazerSpectrum(lazer.color, lazer.strength),
 		};
-		traceBeam(beam, ribs, world, res);
+		traceBeam(beam, ribs, world, res, 1, false, data);
 	}
+	world.oldRibs = res;
 	return res;
 }
 
@@ -453,7 +725,8 @@ function renderBeams(beams, stage) {
 	for (var i = 0; i < beams.length; ++i) {
 		var beam = beams[i];
 		var bg = new PIXI.Graphics();
-		bg.lineStyle(3, beam.color, beam.strength);
+		var cs = getLazerColorStrength(beam.color);
+		bg.lineStyle(3, cs.col, cs.str);
 		bg.moveTo(beam.from.x, beam.from.y);
 		bg.lineTo(beam.to.x, beam.to.y);
 		stage.beams.push(bg);
@@ -493,6 +766,7 @@ function runGame() {
 	    var renderer = new PIXI.CanvasRenderer(640, 480, document.getElementById("game-scene"));
 
 	    stage.beams = [];
+	    stage.dots = [];
 
 	    var levelData = levels[1];
 
@@ -502,7 +776,9 @@ function runGame() {
 	    		updateGraphics(levels[1].prismas[i].polygon);
 	    	}
 	    	for (var i = 0; i < levels[1].targets.length; ++i) {
-	    		updateGraphics(levels[1].targets[i].target);
+	    		var obj = levels[1].targets[i];
+	    		updateGraphics(obj.target);
+	    		obj.filled = 0;
 	    	}
 	    	for (var i = 0; i < levels[1].lasers.length; ++i) {
 	    		updateGraphics(levels[1].lasers[i].lazer);
@@ -514,8 +790,34 @@ function runGame() {
 	    	}
 	    	stage.beams = [];
 
-	    	renderBeams(calculateBeams(levelData, world), stage);
-			renderer.render(stage);
+	    	var beams = calculateBeams(levelData, world);
+	    	if (beams !== false) {
+	    		renderBeams(beams, stage);
+	    	}
+
+	    	// clear beams
+	    	for (var i = 0; i < stage.dots.length; ++i) {
+	    		stage.removeChild(stage.dots[i]);
+	    	}
+	    	stage.dots = [];
+	    	for (var i = 0; i < levels[1].targets.length; ++i) {
+	    		var obj = levels[1].targets[i];
+	    		if (obj.filled == 0) {
+	    			continue;
+	    		}
+
+	    		var graphics = new PIXI.Graphics();
+				graphics.position = new PIXI.Point(obj.position.x, obj.position.y);
+				graphics.lineStyle(0, obj.color, 1);
+				graphics.beginFill(obj.color, 1);
+				graphics.drawCircle(0, 0, (obj.radius - 1) * (obj.filled > 1 ? 1 : obj.filled));
+				graphics.endFill();
+				stage.addChild(graphics);
+				stage.dots.push(graphics);
+	    	}
+
+	    	renderer.render(stage);
+			world.anyChanges = false;
 		});
 
 		loadLevel(levelData, stage, world);
